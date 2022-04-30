016dff8a6b55d8cbc184de156e4ae5f6c6562b7c
George <laos_2111@yahoo.gr>
2022-05-01 01:41:28 +0300

    add check for .env file

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
     if(extension_loaded("mongodb")) {
         require_once __DIR__ . '/vendor/autoload.php';
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
+        if (file_exists(__DIR__ . '/.env')) {
+            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+            $dotenv->load();
+        }
         $manager = new MongoDB\Driver\Manager($_ENV["dbUrl"]);
         $db = (new MongoDB\Client)->{$_ENV["dbName"]};
     }
