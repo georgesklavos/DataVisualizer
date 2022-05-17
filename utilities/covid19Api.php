bd769821e2a54aec163a9bee14400b9bbe935e3c
George <laos_2111@yahoo.gr>
2022-05-17 21:45:39 +0300

    test deploy

--- utilities/covid19Api.php
+++ utilities/covid19Api.php
@@ -1,7 +1,7 @@
 <?php 
     //Create client for the covid19 api
     require_once $_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php";
-    use GuzzleHttp\Client;
+    use GuzzleHttp\Client; 
     $client = new Client(['verify' => false,'base_uri' => $_ENV["covidUrl"]]);   
 
 ?>
